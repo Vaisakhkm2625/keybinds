75a5edc29f158d78c4525c844be745253eff4c9f
Vaisakh K M <vaisakhkm2625@gmail.com>
2023-07-22 02:12:46 +0530

    seprated sidenav

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,34 +1,12 @@
 <script>
-	import { fade, fly } from 'svelte/transition';
 	import CheckBox from './CheckBox.svelte';
-	let list = [
-		{ tabname: 'My Keybindings', role: 'folder' },
-		{ tabname: 'LazyVim', role: 'keybindlist' },
-		{ tabname: 'Neovim distributions', role: 'folder' },
-		{ tabname: 'LazyVim', role: 'keybindlist' },
-		{ tabname: 'LunarVim', role: 'keybindlist' },
-		{ tabname: 'AstroNvim', role: 'keybindlist' },
-		{ tabname: 'WM default bindings', role: 'folder' }
-	];
+	import SideNav from './SideNav.svelte';
 	let visible = true;
 </script>
 
 <div class="container">
 	{#if visible}
-		<div
-			class="item sidebar"
-			in:fly={{ x: -200, duration: 200 }}
-			out:fly={{ x: -200, duration: 200 }}
-		>
-			{#each list as item}
-				{#if item.role == 'folder'}
-					<div class="folder">{item.tabname}</div>
-				{:else}
-					<div class="keybindlist">{item.tabname}</div>
-					<!-- else content here -->
-				{/if}
-			{/each}
-		</div>
+		<SideNav />
 	{/if}
 	<div class="close-btn">
 		<CheckBox bind:visible />
@@ -37,6 +15,11 @@
 </div>
 
 <style>
+	.item {
+		margin: 10px;
+		border-radius: 20px;
+		background-color: #393053;
+	}
 	.container {
 		height: 100vh;
 		width: 100vw;
@@ -47,38 +30,6 @@
 		padding: 10px;
 		justify-content: space-evenly;
 	}
-	.item {
-		margin: 10px;
-		border-radius: 20px;
-		background-color: #393053;
-	}
-
-	.sidebar {
-		flex-grow: 1;
-		display: flex;
-		justify-content: flex-start;
-		flex-direction: column;
-		padding: 10px;
-		gap: 10px;
-	}
-	.folder {
-		background-color: #635985;
-		border-radius: 10px;
-		color: white;
-		padding: 5px;
-		font-size: 1.5rem;
-	}
-
-	.keybindlist {
-		background-color: #635985;
-		border-radius: 10px;
-		align-self: flex-end;
-		color: white;
-		width: 90%;
-		padding: 5px;
-		font-size: 1.5rem;
-	}
-
 	.close-btn {
 		position: fixed;
 		left: 30px;
---
+++ src/routes/SideNav.svelte
@@ -0,0 +1,63 @@
+<script>
+	import { fly } from 'svelte/transition';
+	import CheckBox from './CheckBox.svelte';
+	let list = [
+		{ tabname: 'My Keybindings', role: 'folder' },
+		{ tabname: 'LazyVim', role: 'keybindlist' },
+		{ tabname: 'Neovim distributions', role: 'folder' },
+		{ tabname: 'LazyVim', role: 'keybindlist' },
+		{ tabname: 'LunarVim', role: 'keybindlist' },
+		{ tabname: 'AstroNvim', role: 'keybindlist' },
+		{ tabname: 'WM default bindings', role: 'folder' }
+	];
+
+	function clicked() {
+		alert('hello');
+	}
+</script>
+
+<div class="item sidebar" in:fly={{ x: -200, duration: 200 }} out:fly={{ x: -200, duration: 200 }}>
+	{#each list as item}
+		{#if item.role == 'folder'}
+			<div class="folder">{item.tabname}</div>
+		{:else}
+			<div class="keybindlist" on:click={clicked}>{item.tabname}</div>
+			<!-- else content here -->
+		{/if}
+	{/each}
+</div>
+
+<style>
+	.item {
+		margin: 10px;
+		border-radius: 20px;
+		background-color: #393053;
+	}
+
+	.sidebar {
+		flex-grow: 1;
+		display: flex;
+		justify-content: flex-start;
+		flex-direction: column;
+		padding: 10px;
+		gap: 10px;
+	}
+	.folder {
+		background-color: #635985;
+		border-radius: 10px;
+		color: white;
+		padding: 5px;
+		margin-top: 5px;
+		font-size: 1.5rem;
+	}
+
+	.keybindlist {
+		background-color: #635985;
+		border-radius: 10px;
+		align-self: flex-end;
+		color: white;
+		width: 90%;
+		padding: 5px;
+		font-size: 1.5rem;
+	}
+</style>
